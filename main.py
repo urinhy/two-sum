a535988a64fad2327a4b16ec646cdf04e6626f58
urinhy <yusung3rd@gmail.com>
2023-03-09 11:16:18 +0000

    add main

---
+++ main.py
@@ -0,0 +1,8 @@
+def twoSum(nums: List[int], target: int) -> List[int]:
+	for i in range(0,len(List)):
+		for j in range(1,len(List)):
+			if List[i]+List[j]==target:
+				break
+		break
+	return
+			
